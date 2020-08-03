e35e9bdc756ad8c2313ff84a1ef2ea2d134a6fcb
huevi <17010423+huevi@users.noreply.github.com>
2020-08-03 13:36:29 +0530

    added new countries

--- scrape.py
+++ scrape.py
@@ -38,6 +38,7 @@ for i, r in regionid_data.iterrows():
     try:
 
         country_trends_df = collect_data(r)
+        country_trends_df["country_orgin"] = r["country"]
         total_trends = total_trends.append(
             country_trends_df, ignore_index=True)
 
